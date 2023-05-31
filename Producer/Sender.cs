1b18417c9856a8572a88c63020f914518aa31909
Lejla1502 <lejlasp@hotmail.com>
2023-05-31 22:36:21 +0200

    Add project files.

---
+++ Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+public class Sender
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using(var connection = factory.CreateConnection()) 
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+
+            var message = "Getting started with .NET RabbitMQ";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish("", "BasicTest", null, body);
+
+            Console.WriteLine("Sent message {0}..", message);
+        }
+
+        Console.WriteLine("Pres [Enter] to exit..");
+        Console.ReadLine();
+    }
+}
